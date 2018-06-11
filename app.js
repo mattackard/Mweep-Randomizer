6b69b9ef1e5b238b0d9d833a35d796db9cb2942f
Matt Ackard <mattackard@gmail.com>
2018-06-11 13:20:26 -0700

    background music plays on key press

--- app.js
+++ app.js
@@ -3,22 +3,29 @@ let $forwardObj = $('#video-forward');          //gets the object containing the
 let $reverseObj = $('#video-reverse');
 let $forward = $('#video-forward').get(0);      //gets the video HTML for use with play() and pause()
 let $reverse = $('#video-reverse').get(0);
+let $bgMusic = $('audio').get(0);
 let rightArrow = 39;    //keycode for left arrow key
 let leftArrow = 37;     //keycode for right arrow key
 
-console.log($reverse.ended);
-$reverse.currentTime = $reverse.duration; //gives the reverse video the "ended" property from start
-$reverseObj.css('z-index', '-1');         //hides the reverse video behind the forward video for the start
-console.log($reverse.ended);
+
+function getItem() {
+  console.log('got an item');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  $reverse.currentTime = $reverse.duration; //gives the reverse video the "ended" property from start
+  $reverseObj.css('z-index', '-1');         //hides the reverse video behind the forward video for the start
+})
 
 $('body').keydown((e) => {
+  $bgMusic.play();
   if (e.which == leftArrow && $reverse.ended) {
     if ( ! $forward.ended ) {
       $forward.play();
     }
     else {                                        //when forward video has ended, move it behind the reverse video,
       $reverse.currentTime = 0;                   //give the player an item, and reset the reverse video to the beginning
-      //getItem();
+      getItem();
       $reverseObj.css('z-index', '1');
       $forwardObj.css('z-index', '-1');
     }
@@ -29,7 +36,7 @@ $('body').keydown((e) => {
     }
     else {                                        //when reverse video has ended, move it behind the forward video,
       $forward.currentTime = 0;                   //give the player an item, and reset the forward video to the beginning
-      //getItem();
+      getItem();
       $forwardObj.css('z-index', '1');
       $reverseObj.css('z-index', '-1');
     }
