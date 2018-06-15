7d8b633454726374787bf14e34acd1ac8c1f2689
Matt Ackard <mattackard@gmail.com>
2018-06-14 17:26:11 -0700

    sets variable with itemlist

---
+++ items.js
@@ -0,0 +1,17 @@
+const items = {
+  "bomb": {
+    "name" : "Bomb",
+    "image" : "media/items/bomb.png",
+    "description" : "Light the fuse and it will blow up"
+  },
+  "bow": {
+    "name" : "Bow",
+    "image" : "media/items/bow.png",
+    "description" : "Use arrows to shoot stuff"
+  },
+  "fireArrow": {
+    "name" : "Fire Arrows",
+    "image" : "media/items/fireArrow.png",
+    "description" : "This arrow is perpetually on fire"
+  }
+};
