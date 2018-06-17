d025f949ee333e8e953ec31382b0e0fb8ba5c8c9
Matt Ackard <mattackard@gmail.com>
2018-06-16 21:00:06 -0700

    addded more items

--- items.js
+++ items.js
@@ -1,17 +1,114 @@
 const items = {
+  "adultOcarina": {
+    "name" : "Blue Ocarina",
+    "image" : "media/items/adultOcarina.png",
+    "description" : "It looks nicer than that other ocarina"
+  },
+  "beans": {
+    "name" : "String Bean",
+    "image" : "media/items/beans.png",
+    "description" : "Herbivores may find this to be a delicious snack"
+  },
+  "bigBombBag": {
+    "name" : "Big Sack",
+    "image" : "media/items/bigBombBag.png",
+    "description" : "Put whatever you want in it"
+  },
+  "bigGoron": {
+    "name" : "Big Sword",
+    "image" : "media/items/bigGoron.png",
+    "description" : "Perhaps the creator is compensating for something?"
+  },
+  "bigWallet": {
+    "name" : "Big Pouch",
+    "image" : "media/items/bigWallet.png",
+    "description" : "Put whatever you want in it"
+  },
+  "blueChicken": {
+    "name" : "Mutant Chicken",
+    "image" : "media/items/blueChicken.png",
+    "description" : "A blue chicken. What powers does it hold?"
+  },
+  "blueClothes": {
+    "name" : "Blue Clothes",
+    "image" : "media/items/blueClothes.png",
+    "description" : "A stylish blue outfit"
+  },
+  "blueJuice": {
+    "name" : "Blue Juice",
+    "image" : "media/items/blueJuice.png",
+    "description" : "What flavor do you think it is?"
+  },
+  "blueMagic": {
+    "name" : "Blue Crystal",
+    "image" : "media/items/blueMagic.png",
+    "description" : "A blue light glows inside it"
+  },
+  "blueMarble": {
+    "name" : "Blue Marble",
+    "image" : "media/items/blueMarble.png",
+    "description" : "What use could this have?"
+  },
   "bomb": {
     "name" : "Bomb",
     "image" : "media/items/bomb.png",
     "description" : "Light the fuse and it will blow up"
+  },
+  "bombChu": {
+    "name" : "Rat Figurine",
+    "image" : "media/items/bombChu.png",
+    "description" : "A small decoration"
+  },
+  "boomerang": {
+    "name" : "Boomerang",
+    "image" : "media/items/boomerang.png",
+    "description" : "Throw it and it will come back to you"
+  },
+  "boots": {
+    "name" : "Boots",
+    "image" : "media/items/boots.png",
+    "description" : "Stylish and practical"
+  },
+  "bottle": {
+    "name" : "Bottle",
+    "image" : "media/items/bottle.png",
+    "description" : "An empty bottle. Please recycle"
   },
   "bow": {
     "name" : "Bow",
     "image" : "media/items/bow.png",
     "description" : "Use arrows to shoot stuff"
   },
+  "bracelet": {
+    "name" : "Gold Bracelet",
+    "image" : "media/items/bracelet.png",
+    "description" : "Bling bling"
+  },
+  "brokenSword": {
+    "name" : "Broken Sword",
+    "image" : "media/items/brokenSword.png",
+    "description" : "Just throw it away"
+  },
+  "bronzeQuiver": {
+    "name" : "Cylindrical Arrow Case",
+    "image" : "media/items/brozeQuiver.png",
+    "description" : "Put your arrows in here"
+  },
+  "bug": {
+    "name" : "Bug in a Bottle",
+    "image" : "media/items/bug.png",
+    "description" : "Captured in a bottle. It may be venemous"
+  },
+
+
   "fireArrow": {
     "name" : "Fire Arrows",
     "image" : "media/items/fireArrow.png",
     "description" : "This arrow is perpetually on fire"
   }
 };
+// "": {
+//   "name" : "",                                //delete this on save
+//   "image" : "media/items/.png",
+//   "description" : ""
+// },
