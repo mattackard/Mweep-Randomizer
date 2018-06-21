b9c7851e0022e245b125e2339eca9fc69bf66a27
Matt Ackard <mattackard@gmail.com>
2018-06-20 21:53:19 -0700

    added text to game start, started function to build inventory on start

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const rightArrow = 39;    //keycode for left arrow key
 const leftArrow = 37;     //keycode for right arrow key
 let gameStarted = false;
 let playerName = 'Anonymous';
+let inventory = [];
 
 
 function getItem() {
@@ -23,23 +24,37 @@ function getItem() {
 }
 
 function startGame() {
-  gameStarted = true;
   playerName = $('#titleScreen input').val() || "Anonymous";    //sets player name to anonymous if no other value is provided
   console.log(playerName);
   $('#titleScreen').fadeOut();
   $bgMusic.play();
+  buildInventory();
+  createText([`Welcome to my domain ${playerName}.`,
+        "My son-in-law needs some more gadgets so he can save the world again.",
+        "My legs don't work like they used to, but I can slide left and right better than anyone in Zora's domain!",
+        "There are always things drifting down Zora's river that ends up to the left or right of me. I think it's time to check it out!"], () => {
+    $textBox.delay(1000).fadeOut(400, createText(["I see something to the left! Quick!"], () => {$textBox.delay(1000).fadeOut();}));                      //choppy fades
+    gameStarted = true;
+  });
 }
 
-function createText(textArray) {
+function createText(textArray, func) {
   $textBox.fadeIn();
+  textArray.unshift(" ");                         //why does it skip the first array index? unshift until i figure it out
   let typed = new Typed('#textBox', {
-    strings: textArray,                                       //why does it skip the first array index?
+    strings: textArray,
     typeSpeed: 50,
     loop: false,
-    onComplete: () => {
-      $textBox.delay(1000).fadeOut();
-    }
-  })
+    onComplete: func
+  });
+}
+
+function buildInventory() {
+  console.log("building the inventory");
+  let count = Math.floor(Math.random() * 50);
+  for(count; count != 0; count--) {
+    console.log(count);
+  }
 }
 
 $modal.hide();    //hides modal when page is loaded
@@ -64,6 +79,7 @@ $('body').keydown((e) => {
       getItem();
       $reverseObj.css('z-index', '1');
       $forwardObj.css('z-index', '-1');
+      createText(["I see something to the right! Quick!"], () => {$textBox.delay(2000).fadeOut();});
     }
   }
   if (e.which == rightArrow  && $forward.ended && gameStarted) {
@@ -75,6 +91,7 @@ $('body').keydown((e) => {
       getItem();
       $forwardObj.css('z-index', '1');
       $reverseObj.css('z-index', '-1');
+      createText(["I see something to the left! Quick!"], () => {$textBox.delay(2000).fadeOut();});
     }
   }
 });
