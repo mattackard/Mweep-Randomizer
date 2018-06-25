6713baebd5ccb93eacd9bd2c8595aebccb6dabfd
Matt Ackard <mattackard@gmail.com>
2018-06-25 09:20:10 -0700

    cleaned up intro text, built inventory disallowing duplicate items

--- app.js
+++ app.js
@@ -11,11 +11,25 @@ const rightArrow = 39;    //keycode for left arrow key
 const leftArrow = 37;     //keycode for right arrow key
 let gameStarted = false;
 let playerName = 'Anonymous';
-let inventory = [];
+let inventory = { };
 
 
 function getItem() {
   let item = items[Object.keys(items)[Math.floor(Math.random() * Object.keys(items).length)]];  //gets a random item (items is inherited from items.js)
+
+  while (inventory.hasOwnProperty(item.name)) {                                                 //gets a new item if the random item is a duplicate
+    item = items[Object.keys(items)[Math.floor(Math.random() * Object.keys(items).length)]];
+  }
+
+  inventory[item.name] = {                                  //adds random unique item to inventory
+    "name": item.name,
+    "image": item.image,
+    "description": item.description
+  };
+  return item;
+}
+
+function itemPopup(item) {
   $('#itemName').text(item['name']);                                                            //sets the text on the modal pop-up
   $('#modal img').attr('src', item['image']);
   $('#itemDescription').text(item['description']);
@@ -31,9 +45,10 @@ function startGame() {
   buildInventory();
   createText([`Welcome to my domain ${playerName}.`,
         "My son-in-law needs some more gadgets so he can save the world again.",
-        "My legs don't work like they used to, but I can slide left and right better than anyone in Zora's domain!",
-        "There are always things drifting down Zora's river that ends up to the left or right of me. I think it's time to check it out!"], () => {
-    $textBox.delay(1000).fadeOut(400, createText(["I see something to the left! Quick!"], () => {$textBox.delay(1000).fadeOut();}));                      //choppy fades
+        "My legs don't work like they used to, but I can slide left and right better than any Zora in my kingdom!",
+        "There are always things drifting down Zora's river that ends up to the left or right of me. I think it's time to check it out!",
+        "I see something to the left! Quick!"], () => {
+    $textBox.delay(1000).fadeOut();
     gameStarted = true;
   });
 }
@@ -45,20 +60,24 @@ function createText(textArray, func) {
     strings: textArray,
     typeSpeed: 50,
     loop: false,
+    startDelay: 1200,
     onComplete: func
   });
 }
 
 function buildInventory() {
   console.log("building the inventory");
-  let count = Math.floor(Math.random() * 50);
+  let count = Math.floor(Math.random() * (Object.keys(items).length - 1));
+  console.log(count);
   for(count; count != 0; count--) {
-    console.log(count);
+    getItem();
   }
+  console.log(inventory);
 }
 
 $modal.hide();    //hides modal when page is loaded
 $textBox.hide();
+console.log(Object.keys(items).length);
 
 $reverse.addEventListener('loadeddata', () => {
   $reverse.currentTime = $reverse.duration;     //gives the reverse video the "ended" property from start
@@ -76,7 +95,7 @@ $('body').keydown((e) => {
     }
     else {                                        //when forward video has ended, move it behind the reverse video,
       $reverse.currentTime = 0;                   //give the player an item, and reset the reverse video to the beginning
-      getItem();
+      itemPopup(getItem());
       $reverseObj.css('z-index', '1');
       $forwardObj.css('z-index', '-1');
       createText(["I see something to the right! Quick!"], () => {$textBox.delay(2000).fadeOut();});
@@ -88,7 +107,7 @@ $('body').keydown((e) => {
     }
     else {                                        //when reverse video has ended, move it behind the forward video,
       $forward.currentTime = 0;                   //give the player an item, and reset the forward video to the beginning
-      getItem();
+      itemPopup(getItem());
       $forwardObj.css('z-index', '1');
       $reverseObj.css('z-index', '-1');
       createText(["I see something to the left! Quick!"], () => {$textBox.delay(2000).fadeOut();});
