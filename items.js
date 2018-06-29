efc1a8d6f6728942f4d05a9c39b1ad665bc95dfc
Matt Ackard <mattackard@gmail.com>
2018-06-28 22:23:26 -0700

    added the rest of the items

--- items.js
+++ items.js
@@ -128,10 +128,250 @@ const items = {
     "name" : "Red Arrow",
     "image" : "media/items/fireArrow.png",
     "description" : "This arrow was converted to a blunt object making it of little use"
+  },
+  "firefly": {
+    "name" : "Firefly bottle",
+    "image" : "media/items/firefly.png",
+    "description" : "A glowing, flying bug resides inside"
+  },
+  "fireMagic": {
+    "name" : "Orange Crystal",
+    "image" : "media/items/fireMagic.png",
+    "description" : "This crystal has a glowing orange light inside it"
+  },
+  "fish": {
+    "name" : "Fish in a bottle",
+    "image" : "media/items/fish.png",
+    "description" : "A delicious fish trapped in a bottle for later consumption. MMMMMMMMMMMMmmmmmmmm!"
+  },
+  "fishingPole": {
+    "name" : "Fish Staff",
+    "image" : "media/items/fishingPole.png",
+    "description" : "The deadly staff is used to stab fish and drag them toward you"
+  },
+  "forestMagic": {
+    "name" : "Green Crystal",
+    "image" : "media/items/forestMagic.png",
+    "description" : "This crystal has a glowing green light inside it"
+  },
+  "frog": {
+    "name" : "Frog",
+    "image" : "media/items/frog.png",
+    "description" : "Ribbit ribbit"
+  },
+  "gerudoMask": {
+    "name" : "Human Mask",
+    "image" : "media/items/gerudoMask.png",
+    "description" : "A carved wooden mask of an unsatisfied Gerudo"
+  },
+  "goldLongGloves": {
+    "name" : "Gold Long-Gloves",
+    "image" : "media/items/goldLongGloves.png",
+    "description" : "Long fingerless gloves. Yellow is quite fashionable"
+  },
+  "goldQuiver": {
+    "name" : "Cylindrical Arrow Case",
+    "image" : "media/items/goldQuiver.png",
+    "description" : "Put your arrows in here...This one is gold!"
+  },
+  "goronMask": {
+    "name" : "Peculiar Onion",
+    "image" : "media/items/goronMask.png",
+    "description" : "A strange onion. Someone has put a face on it"
+  },
+  "greenClothes": {
+    "name" : "Green Clothes",
+    "image" : "media/items/greenClothes.png",
+    "description" : "A stylish green outfit"
+  },
+  "greenJuice": {
+    "name" : "Green Juice",
+    "image" : "media/items/greenJuice.png",
+    "description" : "A mysterious green liquid"
+  },
+  "greenMarble": {
+    "name" : "Green Marble",
+    "image" : "media/items/greenMarble.png",
+    "description" : "A collectible green sphere"
+  },
+  "hammer": {
+    "name" : "Hammer",
+    "image" : "media/items/hammer.png",
+    "description" : "A big sledgehammer. Hope you don't need to remove any nails"
+  },
+  "hookshot": {
+    "name" : "Chain Motor",
+    "image" : "media/items/hookshot.png",
+    "description" : "Hook it to something and the chain will retract"
+  },
+  "hoverBoots": {
+    "name" : "Fly Boots",
+    "image" : "media/items/hoverBoots.png",
+    "description" : "Boots with cool looking wings on them"
+  },
+  "hyruleShield": {
+    "name" : "Metal Shield",
+    "image" : "media/items/hyruleShield.png",
+    "description" : "A metal shield with some graphics on it"
+  },
+  "iceArrow": {
+    "name" : "Blue Arrow",
+    "image" : "media/items/iceArrow.png",
+    "description" : "This arrow was converted to a blunt object making it of little use"
+  },
+  "ironBoots": {
+    "name" : "Steel Toe Boots",
+    "image" : "media/items/ironBoots.png",
+    "description" : "Where these and your toes will stay healthy"
+  },
+  "kidOcarina": {
+    "name" : "Holey Wood",
+    "image" : "media/items/kidOcarina.png",
+    "description" : "A small wooden instrument"
+  },
+  "kokiriShield": {
+    "name" : "Wooden Shield",
+    "image" : "media/items/kokiriShield.png",
+    "description" : "It's fairly heavy. Water can weaken this shield"
+  },
+  "kokiriSword": {
+    "name" : "Shank Sword",
+    "image" : "media/items/kokiriSword.png",
+    "description" : "A short sword. Perhaps suitable as a children's toy"
+  },
+  "lens": {
+    "name" : "Magic Wand",
+    "image" : "media/items/lens.png",
+    "description" : "The conspirator's magnifying glass"
+  },
+  "lightArrow": {
+    "name" : "Yellow Arrow",
+    "image" : "media/items/lightArrow.png",
+    "description" : "This arrow was converted to a blunt object making it of little use"
+  },
+  "longshot": {
+    "name" : "Jackhammer",
+    "image" : "media/items/longshot.png",
+    "description" : "Put it between your legs and smash some concrete"
+  },
+  "masterSword": {
+    "name" : "Sword",
+    "image" : "media/items/masterSword.png",
+    "description" : "A standard sword. It doesn't look significant to me"
+  },
+  "message": {
+    "name" : "Message in a bottle",
+    "image" : "media/items/message.png",
+    "description" : "What could it say?"
+  },
+  "milk": {
+    "name" : "Milk",
+    "image" : "media/items/milk.png",
+    "description" : "This weighty berverage makes your bones very strong"
+  },
+  "mirrorShield": {
+    "name" : "Chrome Shield",
+    "image" : "media/items/mirrorShield.png",
+    "description" : "A very shiny shield with red trim"
+  },
+  "mushroom": {
+    "name" : "Mushroom",
+    "image" : "media/items/mushroom.png",
+    "description" : "Beware, it might be poisonous"
+  },
+  "nut": {
+    "name" : "Nut",
+    "image" : "media/items/nut.png",
+    "description" : "You got a nut!"
+  },
+  "pika": {
+    "name" : "Mouse Mask",
+    "image" : "media/items/pika.png",
+    "description" : "A shockingly bold mouse mask. It won't fool anyone"
+  },
+  "poe1": {
+    "name" : "Unusual bottle",
+    "image" : "media/items/poe1.png",
+    "description" : "There is a small man inside this bottle"
+  },
+  "poe2": {
+    "name" : "Unusual bottle",
+    "image" : "media/items/poe2.png",
+    "description" : "There is a small woman inside this bottle"
+  },
+  "redClothes": {
+    "name" : "Red Clothes",
+    "image" : "media/items/redClothes.png",
+    "description" : "A stylish red outfit"
+  },
+  "redJuice": {
+    "name" : "Red Juice",
+    "image" : "media/items/redJuice.png",
+    "description" : "Quite a horrific sight to see"
+  },
+  "saw": {
+    "name" : "Saw Stick",
+    "image" : "media/items/saw.png",
+    "description" : "A saw that is designed as a sword. It looks impractical"
+  },
+  "seeds": {
+    "name" : "Nuts",
+    "image" : "media/items/seeds.png",
+    "description" : "You got some nuts!"
+  },
+  "silverLongGloves": {
+    "name" : "Silver Long Gloves",
+    "image" : "media/items/silverLongGloves.png",
+    "description" : "Long fingerless gloves. The shiny coating will get people looking"
+  },
+  "skullMask": {
+    "name" : "Skull Mask",
+    "image" : "media/items/skullMask.png",
+    "description" : "Emulate a decomposed head with this mask"
+  },
+  "sling": {
+    "name" : "Nut Launcher",
+    "image" : "media/items/sling.png",
+    "description" : "Launch some nuts with this sling"
+  },
+  "smallBombBag": {
+    "name" : "Small Sack",
+    "image" : "media/items/smallBombBag.png",
+    "description" : "Just put some stuff in it"
+  },
+  "smallWallet": {
+    "name" : "Small Pouch",
+    "image" : "media/items/smallWallet.png",
+    "description" : "There's very little that could make this useful"
+  },
+  "spookMask": {
+    "name" : "Spooky Mask",
+    "image" : "media/items/spookMask.png",
+    "description" : "This mask is scary"
+  },
+  "starJuice": {
+    "name" : "Fancy Perfume",
+    "image" : "media/items/starJuice.png",
+    "description" : "It has a feminine aroma"
+  },
+  "stick": {
+    "name" : "Stick",
+    "image" : "media/items/stick.png",
+    "description" : "Not anything worth noting"
+  },
+  "swordFossil": {
+    "name" : "Sword Fossil",
+    "image" : "media/items/swordFossil.png",
+    "description" : "A fossilized sword. What era could this come from?"
+  },
+  "truthMask": {
+    "name" : "Cyclops Mask",
+    "image" : "media/items/truthMask.png",
+    "description" : "Make your friends believe you only have one eye"
+  },
+  "zoraMask": {
+    "name" : "Zora Mask",
+    "image" : "media/items/zoraMask.png",
+    "description" : "I find this offensive"
   }
 };
-// "": {
-//   "name" : "",                                //delete this on save
-//   "image" : "media/items/.png",
-//   "description" : ""
-// },
